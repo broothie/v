70fb01103b4d5c375b9dcb6b81dbf1877b91f589
broothie <adbooth8@gmail.com>
2022-08-11 11:28:14 -0700

    write doctype

--- v.go
+++ v.go
@@ -8,6 +8,12 @@ import (
 func Render(w http.ResponseWriter, statusCode int, nodes ...Node) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(statusCode)
+
+	if _, err := w.Write([]byte("<!doctype html>")); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	if _, err := WriteHTML(w, nodes...); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
